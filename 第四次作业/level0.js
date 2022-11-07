3fdec1fcff2d6b1980c1c060828992e47fca08b2
mhllx <781764699@qq.com>
2022-11-07 21:20:47 +0800

    第四次作业

---
+++ 第四次作业/level0.js
@@ -0,0 +1,24 @@
+const stu = {
+    name: 'MING',
+    hobby: ['play', 'run', 'sing'],
+    address: {
+        school: 'ChongQing',
+        home: 'HENAN'
+    },
+    title: ['student',{year:2022}],
+    skills: {
+        speak() {
+            this.name = 'JACK';
+        }
+    }
+}
+const {name} = stu;
+const {hobby:[h1,h2,h3]} = stu;
+const {address:{home:HOME}} =stu;
+const {title:[,{year}]} = stu;
+const {skills:{speak}} = stu; 
+const {log} = console;
+log(name)  // MING
+log(h1,h2,h3)  // play run sing
+log(HOME)  // HENAN，这里的HOME就是大写哈
+log(year)  // 2022
---
+++ 第四次作业/level1.js
@@ -0,0 +1,21 @@
+//for循环
+function SUM(n) {
+    let sum = 0;
+    for (var i = 1; i <= n; i++) {
+        sum = sum + i;
+    }
+    console.log(sum)
+}
+
+SUM(100)
+
+//递归
+const fn = (a) => {
+    if (a == 1) {
+        return a;
+    }
+    else {
+        return a + fn(a - 1);
+    }
+}
+console.log(fn(100))
